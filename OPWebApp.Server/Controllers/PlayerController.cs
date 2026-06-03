e392cc7ddb18803f9db35347b0476ab1a0eea83b
Stringsnapper <joeme244@gmail.com>
2026-06-03 21:53:17 +0200

    feat(Player): scaffolded simple Player Controller

---
+++ OPWebApp.Server/Controllers/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OPWebApp.Server.Database;
+using OPWebApp.Server.Models;
+
+namespace OPWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly PlayerDb _context;
+
+        public PlayerController(PlayerDb context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns all players in the database.
+        /// </summary>
+        /// <remarks>GET: api/Player</remarks>
+        /// <returns>All available Player data or an empty collection if no player data is available.</returns>
+        /// <response code="200">Returns a collection of Player data.</response>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PlayerModel>>> GetPlayers()
+        {
+            return await _context.Players.ToListAsync();
+        }
+
+        // GET: api/Player/{<GUID>}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PlayerModel>> GetPlayerModel(Guid id)
+        {
+            var playerModel = await _context.Players.FindAsync(id);
+
+            if (playerModel == null)
+            {
+                return NotFound();
+            }
+
+            return playerModel;
+        }
+
+        // PUT: api/Player/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayerModel(Guid id, PlayerModel playerModel)
+        {
+            if (id != playerModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(playerModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Player
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PlayerModel>> PostPlayerModel(PlayerModel playerModel)
+        {
+            _context.Players.Add(playerModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayerModel", new { id = playerModel.Id }, playerModel);
+        }
+
+        // DELETE: api/Player/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayerModel(Guid id)
+        {
+            var playerModel = await _context.Players.FindAsync(id);
+            if (playerModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Players.Remove(playerModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlayerModelExists(Guid id)
+        {
+            return _context.Players.Any(e => e.Id == id);
+        }
+    }
+}
--- OPWebApp.Server/Program.cs
+++ OPWebApp.Server/Program.cs
@@ -1,49 +1,24 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OPWebApp.Server.Database;
 using OPWebApp.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Use in-memory database during development
-builder.Services.AddDbContext<PlayerDb>(opt => opt.UseInMemoryDatabase("Players"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<PlayerDb>(opt => opt.UseInMemoryDatabase("Players"));
+
 var app = builder.Build();
 
-// Minimal api setup 
-// TODO: Remove this in favor of controllers
-app.MapGet("/players", async (PlayerDb db) => await db.Players.ToListAsync());
-
-app.MapPost("/players", async (PlayerModel player, PlayerDb db) => 
+if ( app.Environment.IsDevelopment())
 {
-    db.Players.Add(player);
-    await db.SaveChangesAsync();
+    app.MapOpenApi();
+}
 
-    return Results.Created($"/players/{player.Id}", player);
-});
-
-app.MapPut("/players/{id}", async (Guid id, PlayerModel inputPlayer, PlayerDb db) =>
-{
-    var player = await db.Players.FindAsync(id);
-    if (player is null) return Results.NotFound();
-
-    player.Name = inputPlayer.Name;
-    player.Level = inputPlayer.Level;
-    player.XP = inputPlayer.XP;
-    player.Description = inputPlayer.Description;
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/players/{id}", async (Guid id, PlayerDb db) =>
-{
-    if (await db.Players.FindAsync(id) is PlayerModel player)
-    {
-        db.Remove(player);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-    return Results.NotFound();
-});
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 
 
