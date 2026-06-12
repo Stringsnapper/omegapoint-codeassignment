cd739336675b1310d771ab31dac9f48500642aee
Stringsnapper <joeme244@gmail.com>
2026-06-12 21:10:04 +0200

    feat(frontend): add hover effect on player card

--- OPWebApp.Frontend/src/components/PlayerCard.tsx
+++ OPWebApp.Frontend/src/components/PlayerCard.tsx
@@ -1,4 +1,4 @@
-import type { CSSProperties } from "react";
+import { useState, type CSSProperties } from "react";
 import type { PlayerProps } from "../interfaces/Player";
 
 interface PlayerCardProps {
@@ -7,46 +7,52 @@ interface PlayerCardProps {
 	giveXpAction?: (playerProps: PlayerProps, amount: number) => Promise<boolean>;
 }
 
-const cardStyle: CSSProperties = {
-	display: 'flex',
-	flexDirection: 'column',
-	borderRadius: '8px',
-	marginBottom: '10px',
-	paddingBottom: '10px',
-	backgroundColor: '#91919111',
-	width: '440px',
-}
-const cardHeaderStyle: CSSProperties = {
-	fontSize: '1.5em',
-	fontWeight: 'bold',
-	marginBottom: '5px',
-	height: '40px',
-	alignItems: 'center',
-	justifyContent: 'center',
-	display: 'flex',
-	borderRadius: '8px 8px 0px 0px',
-	backgroundColor: '#443f3fff',
-	paddingLeft: '10px',
-}
 
-const headerButtonStyle: CSSProperties = {
-	marginLeft: 'auto', 
-	marginRight: '10px'
-}
 
-const headerStyle: CSSProperties = {
-	fontSize: '1em',
-	fontWeight: 'bold',
-	marginBottom: '5px',
-	marginRight: '10px',
-	overflow: 'hidden',
-	textOverflow: 'ellipsis',
-	whiteSpace: 'nowrap',
-}
 
 const PlayerCard: React.FC<PlayerCardProps> = props => {
 
+	const [hover, setHover] = useState(false);
 	var playerProps = props.playerProps;
+
+	const cardStyle: CSSProperties = {
+		display: 'flex',
+		flexDirection: 'column',
+		borderRadius: '8px',
+		border: hover ? "solid #91919150" : "solid rgba(0, 0, 0, 0)",
+		marginBottom: '10px',
+		paddingBottom: '10px',
+		backgroundColor: '#91919112',
+		width: '440px',
+	}
+	const cardHeaderStyle: CSSProperties = {
+		fontSize: '1.5em',
+		fontWeight: 'bold',
+		marginBottom: '5px',
+		height: '40px',
+		alignItems: 'center',
+		justifyContent: 'center',
+		display: 'flex',
+		borderRadius: '8px 8px 0px 0px',
+		backgroundColor: '#443f3fff',
+		paddingLeft: '10px',
+	}
+
+	const headerButtonStyle: CSSProperties = {
+		marginLeft: 'auto',
+		marginRight: '10px'
+	}
+
+	const headerStyle: CSSProperties = {
+		fontSize: '1em',
+		fontWeight: 'bold',
+		marginBottom: '5px',
+		marginRight: '10px',
+		overflow: 'hidden',
+		textOverflow: 'ellipsis',
+		whiteSpace: 'nowrap',
+	}
+
 
 	const handleDelete = async () => {
 		if (props.deleteAction) {
@@ -60,8 +66,8 @@ const PlayerCard: React.FC<PlayerCardProps> = props => {
 		}
 	}
 
-	return(
-		<div style={cardStyle}>
+	return (
+		<div style={cardStyle} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
 			<div style={cardHeaderStyle}>
 				<h2 style={headerStyle} title={playerProps.name}>{playerProps.name}</h2>
 				<button onClick={handleGiveXp} style={headerButtonStyle}>Give XP</button>
